4fcbfdf761cd35e63eb9a5245f6e78f95b8b4374
Nhan Nguyen <nathan13574@gmail.com>
2020-06-27 18:17:19 -0500

    delete

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -2,13 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { MessageComponent } from './message/message.component';
 
-const routes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'contact', component: ContactComponent },
-	{ path: 'message', component: MessageComponent }
-];
+const routes: Routes = [ { path: '', component: HomeComponent }, { path: 'contact', component: ContactComponent } ];
 
 @NgModule({
 	imports: [ RouterModule.forRoot(routes) ],
