36905505dcd9643cd7512aea4fef49e441c494e5
Nhan Nguyen <nathan13574@gmail.com>
2020-06-27 04:58:13 -0500

    ok

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-PROD_MONGODB = 'mongodb://localhost/Hiring';
+require('dotenv').config({
+	path: './.env'
+});
 
 const app = express();
 mongoose.connect(process.env.DATABASE_URL, {
